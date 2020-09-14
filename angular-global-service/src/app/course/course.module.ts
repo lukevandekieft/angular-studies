38ce6627427514f200c45abee7e21a148f5e3b77
lukevandekieft <vandekie@gmail.com>
2020-09-14 16:57:30 -0700

    Create new store service.

--- angular-global-service/src/app/course/course.module.ts
+++ angular-global-service/src/app/course/course.module.ts
@@ -1,13 +1,8 @@
-import { CourseEffects } from './store/course.effects';
-import { CourseService } from './services/course.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { CoursesListComponent } from './component/courses-list/courses-list.component';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { courseReducer } from './store/course.reducers';
 import { CreateCourseComponent } from './component/create-course/create-course.component';
 
 @NgModule({
@@ -17,11 +12,9 @@ import { CreateCourseComponent } from './component/create-course/create-course.c
   ],
   imports: [
     CommonModule,
-    FormsModule,
-    StoreModule.forFeature('courses', courseReducer),
-    EffectsModule.forFeature([CourseEffects])
+    FormsModule
   ],
-  providers: [CourseService],
+  // providers: [CourseService],
   bootstrap: [],
   exports: [CoursesListComponent, CreateCourseComponent]
 })
---
+++ angular-global-service/src/app/store/course-store.service.ts
@@ -0,0 +1,37 @@
+// todos-store.service.ts
+import { Course } from '../course/model/course.model';
+import { CourseModule } from '../course/course.module';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class CoursesStoreService {
+  // Make _coursesSource private so it's not accessible from the outside, 
+  // expose it as courses$ observable (read-only) instead.
+  // Write to _coursesSource only through specified store methods below.
+  private readonly _coursesSource = new BehaviorSubject<Course[]>([]);
+
+  // Exposed observable (read-only).
+  readonly courses$ = this._coursesSource.asObservable();
+
+  constructor() {}
+
+  // Get last value without subscribing to the courses$ observable (synchronously).
+  getCourses(): Course[] {
+    return this._coursesSource.getValue();
+  }
+
+  private _setPuppies(courses: Course[]): void {
+    this._coursesSource.next(courses);
+  }
+
+  addCourse(course: Course): void {
+    const courses = [...this.getCourses(), course];
+    this._setPuppies(courses);
+  }
+
+  removeCourse(course: Course): void {
+    const courses = this.getCourses().filter(p => p.id !== course.id);
+    this._setPuppies(courses);
+  }
+}
