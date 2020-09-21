3494a5b5c4022297745a71ff4063b648d8efb7e8
lukevandekieft <vandekie@gmail.com>
2020-09-21 10:30:44 -0700

    Create product list HTML template.

--- angular-pluralsight-intro/APM-Start/src/app/app.component.ts
+++ angular-pluralsight-intro/APM-Start/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
-@Component({ // decorators tell Angular what this actually is
+@Component({ // decorators tell Angular what this actually is (component? Module? Other?)
   selector: 'pm-root',
   templateUrl: './app.component.html',
+  // template: `<div></div>`, <-- Inline template
   styleUrls: ['./app.component.css']
 })
 
