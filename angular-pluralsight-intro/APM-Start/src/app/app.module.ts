437cf2346c0767183c189fca48edd602863070c8
lukevandekieft <vandekie@gmail.com>
2020-09-21 11:21:47 -0700

    Create and import product-list component.

--- angular-pluralsight-intro/APM-Start/src/app/app.module.ts
+++ angular-pluralsight-intro/APM-Start/src/app/app.module.ts
@@ -2,14 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({ // @NgModule decorator is what tells Angular to interpret this class as a module
   declarations: [ // List of all components in module
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule // List of supporting modules
   ],
-  bootstrap: [AppComponent] // The component our app should start fromt
+  bootstrap: [AppComponent] // The component our app should start from
 })
 export class AppModule { }
