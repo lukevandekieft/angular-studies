18fd7eb62197bb0f0987fdc69bfa92b7e3b629c3
lukevandekieft <vandekie@gmail.com>
2020-09-22 17:05:25 -0700

    Create routes for navigation.

--- angular-pluralsight-intro/APM-Start/src/app/app.module.ts
+++ angular-pluralsight-intro/APM-Start/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
@@ -22,7 +23,14 @@ import { WelcomeComponent } from './home/welcome.component';
   imports: [
     BrowserModule, // List of supporting modules
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id', component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' } // typically a 404 page
+    ])
   ],
   bootstrap: [AppComponent] // The component our app should start from
 })
