ca0c241ec15fea04c8beff2cbed691bd05e65c06
lukevandekieft <vandekie@gmail.com>
2020-10-02 14:45:52 -0700

    Show API data on individual detail pages.

--- pluralsight-intro/APM-Start/src/app/products/product-detail/product-detail.component.ts
+++ pluralsight-intro/APM-Start/src/app/products/product-detail/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../product.service';
 
 import { IProduct } from '../product';
 
@@ -11,13 +12,21 @@ import { IProduct } from '../product';
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
+  errorMessage: string;
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  constructor(private route: ActivatedRoute, private router: Router, private productService: ProductService) { }
 
   ngOnInit(): void {
     let productId = +this.route.snapshot.paramMap.get('productId');
-    console.log(productId)
     this.pageTitle += `: ${productId}`;
+
+    this.productService.getProducts().subscribe({
+      next: arbitraryVariableName => {
+        let filteredProducts = arbitraryVariableName.filter(productInstance => productInstance.productId == productId);
+        this.product = filteredProducts[0];
+      },
+      error: err => this.errorMessage = err
+    });
   }
 
   onBack(): void {
